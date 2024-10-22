db95edf9556dbfbd1bac0c131a75a9d1966ac73d
PratoGamer <gaboprato33@gmail.com>
2024-10-22 19:17:52 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+/* 	Autores:
+	-> Luis Rojas
+	-> Gabriel Prato
+	-> 
+	
+	Entrega:
+	31-10-24
+*/
+
+
+int main(int argc, char** argv) {
+	
+	string nombre = "Gabriel";
+	
+	cout << nombre;
+	
+	exit(0);
+}
