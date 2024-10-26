c982e6c60755485dc1d99cd0bfba9cb7fc9e792d
LuisR16B <131700462+LuisR16B@users.noreply.github.com>
2024-10-26 18:18:04 -0400

    submenus

--- Aplicacion.cpp
+++ Aplicacion.cpp
@@ -13,10 +13,9 @@ Aplicacion::Aplicacion(){
 
 // Menu Principal
 void Aplicacion::Menu(){
-	cout << "\t *** Bienvenido a SpeedCar ***" << endl;
-	cout << endl;
-	
 	while(true){
+		cout << "\t *** Bienvenido a SpeedCar ***" << endl;
+		cout << endl;
 		cout << "\t 1 -> Usuario." << endl;
 		cout << "\t 2 -> Servicio Diario." << endl;
 		cout << "\t 3 -> Finalizar Dia." << endl;
@@ -24,7 +23,7 @@ void Aplicacion::Menu(){
 		
 		cout << "\t Ingrese su Seleccion: ";
 		cin >> this->eleccion;
-		
+		system("cls");
 		switch(this->eleccion) {
 			case 1:
 				this->MenuUsuario();
@@ -56,13 +55,73 @@ void Aplicacion::MenuUsuario(){
 	cout << "\t *** Menu de Usuario ***" << endl;
 	cout << endl;
 	
+	cout << "\t 1 -> Agregar." << endl;
+	cout << "\t 2 -> Modificar." << endl;
+	cout << "\t 3 -> Consultar." << endl;
+	cout << "\t 4 -> Eliminar." << endl;
+	cout << "\t 5 -> Salir." << endl;
+	cout << endl;
+	cout << "\t Ingrese su Seleccion: ";
+	cin >> this->eleccion;
 	
+	switch(this->eleccion) {
+		case 1:
+		   	break;
+		   	
+		case 2:
+		   	break;
+		    	
+		case 3:
+		   	break;
+		   	
+		case 4:
+		   	break;
+		
+		case 5:
+		   	break;
+		   	
+		default:
+		  	cout << "\t Seleccion no Valida" << endl;
+		  	cout << endl;
+		}
+		system("cls");
 }
 
 // Menu de Servicio Diario
 void Aplicacion::MenuServicioDiario(){
 	cout << "\t *** Menu de Servicio Diario ***" << endl;
 	cout << endl;
+	
+	cout << "\t 1 -> Actualizar ubicacion del vehiculo." << endl;
+	cout << "\t 2 -> Solicitar Traslado." << endl;
+	cout << "\t 3 -> Seleccionar vehículo." << endl;
+	cout << "\t 4 -> Finalizar traslado." << endl;
+	cout << "\t 5 -> Salir." << endl;
+	cout << endl;
+	cout << "\t Ingrese su Seleccion: ";
+	cin >> this->eleccion;
+	
+	switch(this->eleccion) {
+		case 1:
+		   	break;
+		   	
+		case 2:
+		   	break;
+		    	
+		case 3:
+		   	break;
+		   	
+		case 4:
+		   	break;
+		
+		case 5:
+		   	break;
+		   	
+		default:
+		  	cout << "\t Seleccion no Valida" << endl;
+		  	cout << endl;
+		}
+		system("cls");
 	
 	
 }
